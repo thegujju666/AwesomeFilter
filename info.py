2ea2c83cd21d8ad884d4d6350e4cd1aa5417866f
Giriraj2505 <98442107+Giriraj2505@users.noreply.github.com>
2023-02-20 16:12:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -35,8 +35,8 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://giri9057:giri9057@cluster0.zflhm.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Filterautobot")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://giri905789:giri905789@cluster0.kkwjf.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "roseautofilter")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
