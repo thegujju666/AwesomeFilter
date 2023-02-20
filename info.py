1fdf98ac74dde84d7fcc5354d784e33ee9d164cd
Giriraj2505 <98442107+Giriraj2505@users.noreply.github.com>
2023-02-20 21:56:22 +0530

    Update info.py

--- info.py
+++ info.py
@@ -67,10 +67,10 @@ LOG_STR += ("Spell Check Mode Is Enabled, bot will be suggesting related movies 
 LOG_STR += (f"MAX_LIST_ELM Found, long list will be shortened to first {MAX_LIST_ELM} elements\n" if MAX_LIST_ELM else "Full List of casts and crew will be shown in imdb template, restrict them by adding a value to MAX_LIST_ELM\n")
 LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
 
-SELF_DELETE_SECONDS = int(environ.get('SELF_DELETE_SECONDS', 0))
-SELF_DELETE = environ.get('SELF_DELETE', True)
-if SELF_DELETE == "True":
-    SELF_DELETE = True
+SELF_DELETE_SECONDS = int(environ.get('SELF_DELETE_SECONDS', 300))
+SELF_DELETE = environ.get('SELF_DELETE', False)
+if SELF_DELETE == "False":
+    SELF_DELETE = False
 
 SHORTENR_SITE = environ.get('SHORTENR_SITE', 'urlshortner.co')
 
