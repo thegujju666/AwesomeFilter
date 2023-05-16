0400cdadf5e2e5890808383b7cedfc3e70163cb2
Giriraj2505 <98442107+Giriraj2505@users.noreply.github.com>
2023-05-16 10:44:08 +0530

    Update info.py

--- info.py
+++ info.py
@@ -33,7 +33,7 @@ AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://giri905789:giri905789@cluster0.kkwjf.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "roseautofilterr")
+DATABASE_NAME = environ.get('DATABASE_NAME', "roseautofilter")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
