4babdd569d00ad9668ece7e3edfb0697004b24b1
Giriraj2505 <98442107+Giriraj2505@users.noreply.github.com>
2023-07-01 06:48:21 +0530

    Update info.py

--- info.py
+++ info.py
@@ -69,6 +69,6 @@ SELF_DELETE = environ.get('SELF_DELETE', False)
 if SELF_DELETE == "False":
     SELF_DELETE = False
 
-SHORTENR_SITE = environ.get('SHORTENR_SITE', 'urlshorten.in')
+SHORTENR_SITE = environ.get('SHORTENR_SITE', 'onepagelink.in')
 
-SHORTNER_API = environ.get('SHORTNER_API', 'a9d2cb31c9346bef2bc4137d81fa1384fde3626f')
+SHORTNER_API = environ.get('SHORTNER_API', '425e47b0e55d9fbfd985a29098fdf59896e8b08a')
