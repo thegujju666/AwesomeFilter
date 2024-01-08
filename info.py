75a9c2f2c16ee943b1d5d8bf08b4c3ebef1bd0fe
thegujju666 <135796788+thegujju666@users.noreply.github.com>
2024-01-09 00:39:13 +0530

    Update info.py

--- info.py
+++ info.py
@@ -69,6 +69,7 @@ SELF_DELETE = environ.get('SELF_DELETE', True)
 if SELF_DELETE == "True":
     SELF_DELETE = True
 
-SHORTNER_SITE = environ.get('SHORTNER_SITE', 'old.omegalinks.in')
+SHORTNER_SITE = environ.get('SHORTNER_SITE', 'api.shareus.io')
 
-SHORTNER_API = environ.get('SHORTNER_API', '980e20ad4ae553bf3a018f02c81c6aab53ca5b00')
+SHORTNER_API = environ.get('SHORTNER_API', 'Rb9AVzYe7Cb3aZcdZ5iE8Ij98Fw2
+')
