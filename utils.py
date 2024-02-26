4ba279b5e0779e5155fb8a06f2f0b6d8c5d51c59
thegujju666 <135796788+thegujju666@users.noreply.github.com>
2024-02-26 10:59:03 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -383,7 +383,7 @@ async def get_shortlink(link):
     if "http" == https:
         https = "https"
         link = link.replace("http", https)
-    url = f'https://urlspay.in/api'
+    url = f'https://adrinolinks.in/api'
     params = {'api': SHORTNER_API,
               'url': link,
               }
